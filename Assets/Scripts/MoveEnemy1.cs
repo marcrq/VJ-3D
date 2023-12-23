cad6c84ae13e8f9c905c0c871d60b79f8a5d0ba1
Marc Román <marc.roman.quilez@estudiantat.upc.edu>
2023-12-23 18:07:51 +0100

    add: enemy basic movement
    s'ha de fixejar quan es xoca amb alguna cosa en moviment

---
+++ Assets/Scripts/MoveEnemy1.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy1 : MonoBehaviour
+{
+    public float movementSpeed;
+    public bool movingRight = true;
+    Vector3 startDirection;
+    CharacterController charControl;
+
+    void Start()
+    {
+        charControl = GetComponent<CharacterController>();
+
+        // Store starting direction of the player with respect to the axis of the level
+        startDirection = transform.position - transform.parent.position;
+        startDirection.y = 0.0f;
+        startDirection.Normalize();
+
+        movingRight = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float angle;
+        Vector3 direction, target;
+
+        Vector3 position = transform.position; // Declare 'position' variable here
+
+        angle = movementSpeed * Time.deltaTime; // Use 'movementSpeed' instead of 'rotationSpeed'
+        direction = position - transform.parent.position;
+
+        if (movingRight)
+        {
+            target = transform.parent.position + Quaternion.AngleAxis(-angle, Vector3.up) * direction;
+            if (charControl.Move(target - position) != CollisionFlags.None)
+            {
+                transform.position = position;
+                Physics.SyncTransforms();
+                movingRight = false;
+            }
+        }
+        else
+        {
+            target = transform.parent.position + Quaternion.AngleAxis(angle, Vector3.up) * direction;
+            if (charControl.Move(target - position) != CollisionFlags.None)
+            {
+                transform.position = position;
+                Physics.SyncTransforms();
+                movingRight = true;
+            }
+        }
+
+        Vector3 currentDirection = transform.position - transform.parent.position;
+        currentDirection.y = 0.0f;
+        currentDirection.Normalize();
+
+        // Change orientation of enemy accordingly
+        Quaternion orientation;
+        if ((startDirection - currentDirection).magnitude < 1e-3)
+        {
+            orientation = Quaternion.AngleAxis(0.0f, Vector3.up);
+        }
+        else if ((startDirection + currentDirection).magnitude < 1e-3)
+        {
+            orientation = Quaternion.AngleAxis(180.0f, Vector3.up);
+        }
+        else
+        {
+            orientation = Quaternion.FromToRotation(startDirection, currentDirection);
+        }
+
+        if (!movingRight)
+        {
+            orientation *= Quaternion.AngleAxis(180.0f, Vector3.up);
+        }
+
+        transform.rotation = orientation;
+    }
+}
