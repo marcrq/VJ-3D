2787ab6d0f96220692b336028a9ae5537d991bd9
Marc Román <marc.roman.quilez@estudiantat.upc.edu>
2023-12-24 18:52:17 +0100

    add: dash

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class MovePlayer : MonoBehaviour
 {
     Animator animator;
+    public LayerMask enemyLayer;
+    public LayerMask defaultLayer;
     public float rotationSpeed, jumpSpeed, gravity;
     public bool running_right, running_left, last_running_left;
     
     bool isOut;
     Vector3 startDirection;
     float speedY;
+
+    bool isDashing, canTakeDamage;
+    float dashDuration, dashTimer;
+    float lastDashTime;
+    float dashCooldown;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +33,17 @@ public class MovePlayer : MonoBehaviour
         running_left = false;
         running_right = false;
         last_running_left = false;
+
+        isDashing = false;
+        canTakeDamage = true;
+        dashDuration = 0.5f;
+        dashTimer = 0.0f;
+
+        enemyLayer = LayerMask.NameToLayer("Enemy");
+        defaultLayer = gameObject.layer;
+
+        dashCooldown = 2.0f;
+        lastDashTime = -dashCooldown;
     }
 
     // Update is called once per frame
@@ -33,63 +51,39 @@ public class MovePlayer : MonoBehaviour
     {
         CharacterController charControl = GetComponent<CharacterController>();
         Vector3 position;
-        running_left = running_right = false;
 
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        // Dash
+        if (Input.GetKey(KeyCode.L) && !isDashing && Time.time - lastDashTime > dashCooldown)
         {
-            Vector3 direction, target, up;
+            isDashing = true;
+            animator.SetBool("dashing", true);
+            dashTimer = 0.0f;
 
-            position = transform.position;
-            direction = position - transform.parent.position;
-            up = new Vector3(0.0f, transform.position.y, 0.0f);
+            canTakeDamage = false;
 
-            if (Input.GetKey(KeyCode.W) && isOut)
-            {
-                target = Vector3.MoveTowards(transform.position, up, 2f);
-                isOut = false;
-                if (charControl.Move(target - position) != CollisionFlags.None)
-                {
-                    transform.position = target;
-                    Physics.SyncTransforms();
-                }
-            }
-            if (Input.GetKey(KeyCode.S) && !isOut)
-            {
-                target = Vector3.MoveTowards(transform.position, up, -2f);
-                isOut = true;
-                if (charControl.Move(target - position) != CollisionFlags.None)
-                {
-                    transform.position = target;
-                    Physics.SyncTransforms();
-                }
-            }
+            Physics.IgnoreLayerCollision(defaultLayer, enemyLayer, true);
+            lastDashTime = Time.time;
         }
 
-
-        // Left-right movement
-        if (Input.GetKey(KeyCode.A) ^ Input.GetKey(KeyCode.D))
+        if (isDashing)
         {
+            dashTimer += Time.deltaTime;
+
             float angle;
             Vector3 direction, target;
 
             position = transform.position;
-            angle = rotationSpeed * Time.deltaTime;
+            angle = rotationSpeed * Time.deltaTime * 2;
             direction = position - transform.parent.position;
-            if (Input.GetKey(KeyCode.A))
-            {
-                running_left = true;
-                last_running_left = true;
+
+            if (last_running_left) {
                 target = transform.parent.position + Quaternion.AngleAxis(angle, Vector3.up) * direction;
                 if (charControl.Move(target - position) != CollisionFlags.None)
                 {
                     transform.position = position;
                     Physics.SyncTransforms();
                 }
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                running_right = true;
-                last_running_left = false;
+            } else {
                 target = transform.parent.position + Quaternion.AngleAxis(-angle, Vector3.up) * direction;
                 if (charControl.Move(target - position) != CollisionFlags.None)
                 {
@@ -97,8 +91,83 @@ public class MovePlayer : MonoBehaviour
                     Physics.SyncTransforms();
                 }
             }
+
+            if (dashTimer >= dashDuration)
+            {
+                isDashing = false;
+                animator.SetBool("dashing", false);
+                Physics.IgnoreLayerCollision(defaultLayer, enemyLayer, false);
+                canTakeDamage = true;
+            }
         }
-        animator.SetBool("running", running_right ^ running_left);
+        else {
+            running_left = running_right = false;
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+            {
+                Vector3 direction, target, up;
+
+                position = transform.position;
+                direction = position - transform.parent.position;
+                up = new Vector3(0.0f, transform.position.y, 0.0f);
+
+                if (Input.GetKey(KeyCode.W) && isOut)
+                {
+                    target = Vector3.MoveTowards(transform.position, up, 2f);
+                    isOut = false;
+                    if (charControl.Move(target - position) != CollisionFlags.None)
+                    {
+                        transform.position = target;
+                        Physics.SyncTransforms();
+                    }
+                }
+                if (Input.GetKey(KeyCode.S) && !isOut)
+                {
+                    target = Vector3.MoveTowards(transform.position, up, -2f);
+                    isOut = true;
+                    if (charControl.Move(target - position) != CollisionFlags.None)
+                    {
+                        transform.position = target;
+                        Physics.SyncTransforms();
+                    }
+                }
+            }
+
+
+            // Left-right movement
+            if (Input.GetKey(KeyCode.A) ^ Input.GetKey(KeyCode.D))
+            {
+                float angle;
+                Vector3 direction, target;
+
+                position = transform.position;
+                angle = rotationSpeed * Time.deltaTime;
+                direction = position - transform.parent.position;
+                if (Input.GetKey(KeyCode.A))
+                {
+                    running_left = true;
+                    last_running_left = true;
+                    target = transform.parent.position + Quaternion.AngleAxis(angle, Vector3.up) * direction;
+                    if (charControl.Move(target - position) != CollisionFlags.None)
+                    {
+                        transform.position = position;
+                        Physics.SyncTransforms();
+                    }
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    running_right = true;
+                    last_running_left = false;
+                    target = transform.parent.position + Quaternion.AngleAxis(-angle, Vector3.up) * direction;
+                    if (charControl.Move(target - position) != CollisionFlags.None)
+                    {
+                        transform.position = position;
+                        Physics.SyncTransforms();
+                    }
+                }
+            }
+            animator.SetBool("running", running_right ^ running_left);
+        }
 
         // Correct orientation of player
         // Compute current direction
@@ -129,7 +198,6 @@ public class MovePlayer : MonoBehaviour
 
         transform.rotation = orientation;
 
-
         // Apply up-down movement
         position = transform.position;
         if (charControl.Move(speedY * Time.deltaTime * Vector3.up) != CollisionFlags.None)
@@ -143,7 +211,7 @@ public class MovePlayer : MonoBehaviour
                 speedY = 0.0f;
                 animator.SetBool("jumping", false);
             }
-            if (Input.GetKey(KeyCode.Space)) {
+            if (Input.GetKey(KeyCode.Space) && !isDashing) {
                 speedY = jumpSpeed;
                 animator.SetBool("jumping", true);
             }
