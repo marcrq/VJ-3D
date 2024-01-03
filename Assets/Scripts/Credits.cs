f2cc9349ec95c1214a91351077d0b4e01b65127f
Eric Riera <ericrp2002@gmail.com>
2024-01-03 23:54:35 +0100

    add: credits & little updates

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -1,14 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
-    public void OnClickBackButton()
-    {
-        SceneManager.LoadScene("Menu");
-    }
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +15,9 @@ public class Credits : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            SceneManager.LoadScene("Menu");
+        }
     }
 }
